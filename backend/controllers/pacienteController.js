5e5f24349f37cfa90307093da6f35f1a1d1054d4
tonypeanut <tonypeanut93@gmail.com>
2024-03-08 15:56:42 -0700

    Backend terminado

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -10,7 +10,75 @@ const agregarPaciente = async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-}
-const obtenerPacientes = (req, res) => {}
+};
 
-export { agregarPaciente, obtenerPacientes };
+const obtenerPacientes = async (req, res) => {
+    const pacientes = await Paciente.find().where("veterinario").equals(req.veterinario);
+    res.json(pacientes);
+};
+
+const obtenerPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+
+    if(!paciente){
+        return res.status(404).json({msg: "No encontrado"});
+    }
+
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: "Acción no válida" });
+    }
+    
+    res.json(paciente);
+}
+
+const actualizarPaciente = async ( req, res ) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+
+    if(!paciente){
+        return res.status(404).json({msg: "No encontrado"});
+    }
+
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: "Acción no válida" });
+    }
+    
+    // Actualizar paciente
+    paciente.nombre = req.body.nombre || paciente.nombre;
+    paciente.propietario = req.body.propietario || paciente.propietario;
+    paciente.email = req.body.email || paciente.email;
+    paciente.fecha = req.body.fecha || paciente.fecha;
+    paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+    try {
+        const pacienteActualizado = await paciente.save();
+        res.json(pacienteActualizado);
+    } catch (error){
+        console.log(error)
+    }
+}
+
+const eliminarPaciente = async ( req, res ) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+    if(!paciente){
+        return res.status(404).json({msg: "No encontrado"});
+    }
+
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: "Acción no válida" });
+    }
+
+    try {
+        await paciente.deleteOne();
+        res.json({msg:"Paciente eliminado"});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export { agregarPaciente, obtenerPacientes, obtenerPaciente, actualizarPaciente, eliminarPaciente };
--- backend/routes/pacienteRoutes.js
+++ backend/routes/pacienteRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import checkAuth from "../middleware/authMiddleware.js";
-import { agregarPaciente, obtenerPacientes } from '../controllers/pacienteController.js'
+import { agregarPaciente, obtenerPacientes, obtenerPaciente, actualizarPaciente, eliminarPaciente } from '../controllers/pacienteController.js'
 
 const router = express.Router();
 
-router.route('/').post(checkAuth, agregarPaciente).get(obtenerPacientes)
+router.route('/').post(checkAuth, agregarPaciente).get(checkAuth, obtenerPacientes);
+router.route('/:id')
+    .get(checkAuth, obtenerPaciente)
+    .put(checkAuth, actualizarPaciente)
+    .delete(checkAuth, eliminarPaciente)
 
 export default router;
