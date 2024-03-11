76b9fb4bcbfaaf94143b2c9a286d048da5194669
tonypeanut <tonypeanut93@gmail.com>
2024-03-11 16:29:23 -0700

    Avances frontend

---
+++ backend/helpers/emailRegistro.js
@@ -0,0 +1,14 @@
+import nodemailer from "nodemailer";
+
+const emailRegistro = () => {
+    var transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
+        }
+    });
+};
+
+export default emailRegistro;
---
+++ frontend/src/pages/ConfirmarCuenta.jsx
@@ -0,0 +1,9 @@
+const ConfirmarCuenta = () => {
+    return (
+      <>
+          <h1>ConfirmarCuenta</h1>
+      </>
+    )
+  }
+  
+  export default ConfirmarCuenta
