8f717d07f9ad4dfcbf0ee0edbbdc4d03fd7bd890
tonypeanut <tonypeanut93@gmail.com>
2024-04-20 16:02:46 -0700

    fix

--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import generarId from '../helpers/generarId.js'
+import { generarId } from '../helpers/generarId.js';
 
 const veterinarioSchema = mongoose.Schema({
     nombre: {
