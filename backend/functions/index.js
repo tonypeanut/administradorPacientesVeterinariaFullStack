f87068c20de0b607bf9c07a6cb9338adf32f1e96
tonypeanut <tonypeanut93@gmail.com>
2024-04-20 15:18:07 -0700

    other fixes

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -5,6 +5,7 @@ import conectarDB from "../config/db.js";
 import veterinarioRoutes from "../routes/veterinarioRoutes.js";
 import pacienteRoutes from "../routes/pacienteRoutes.js";
 import serverless from 'serverless-http'
+import router from "../routes/veterinarioRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -40,5 +41,6 @@ app.listen(4000, ()=>{
     console.log(`Servidor funcionando en el puerto ${PORT}`)
 })
 
+app.use('/.netlify/functions/server', router)
 export const handler = serverless(app);
 
