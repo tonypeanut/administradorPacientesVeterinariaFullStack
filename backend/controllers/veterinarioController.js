9acad1f609e31db5453a8ae2cdea3f374aec9782
tonypeanut <tonypeanut93@gmail.com>
2024-04-20 16:00:38 -0700

    fix

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,6 +1,6 @@
 import Veterinario from "../models/Veterinario.js"
 import generarJWT from "../helpers/generarJWT.js";
-import generarId from "../helpers/generarId.js";
+import { generarId } from "../helpers/generarId.js";
 import emailRegistro from "../helpers/emailRegistro.js";
 import emailOlvidePassword from "../helpers/emailOlvidePassword.js";
 
